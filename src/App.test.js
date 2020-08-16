10c9c3ce7a8c276d51fcb1b0a77633cd6bbcf606
Maciej Lenartowicz <mcjlnrtwcz@gmail.com>
2020-08-16 16:24:06 +0200

    Clear/delete tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,34 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import App from "./App";
 
-test("renders header", () => {
+test("deletes a character", () => {
   const { getByText } = render(<App />);
-  const header = getByText(/PhoneWords/i);
-  expect(header).toBeInTheDocument();
+  const inputButton = getByText("2");
+  const deleteButton = getByText("DEL");
+  fireEvent.click(inputButton);
+  fireEvent.click(inputButton);
+  const screen = getByText("22");
+  fireEvent.click(deleteButton);
+  expect(screen.innerHTML).toBe("2");
 });
+
+test("clears screen", () => {
+  const { getByText } = render(<App />);
+  const inputButton = getByText("3");
+  const clearButton = getByText("CLR");
+  fireEvent.click(inputButton);
+  fireEvent.click(inputButton);
+  fireEvent.click(inputButton);
+  const screen = getByText("333");
+  fireEvent.click(clearButton);
+  expect(screen.innerHTML).toBe("");
+});
+
+test("display found predictions", () => {});
+
+test("handle no predictions", () => {});
+
+test("handle loading", () => {});
+
+test("handle API error", () => {});
